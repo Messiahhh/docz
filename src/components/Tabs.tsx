2c8f962d378ff5936946d47227478e1304c24bc5
messiahhh <616348105@qq.com>
2020-05-28 22:33:07 +0800

    部署

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -2,64 +2,53 @@ import * as React from 'react'
 import { 
     useState, 
     useEffect,
-    useRef,
 } from 'react'
-import './test.scss'
+
+import './index.scss'
+
 const classNames = require('classnames')
 
-type flexDirection =  "left" | "right" | "top" | "bottom"
-
+type position =  "left" | "right" | "top" | "bottom"
+type size = 'small' | 'default' | 'large'
 
 interface TabsProps {
+    defaultActiveIndex?: string,
     type?: 'line' | 'card',
-    defaultActiveKey?: string,
-    onChange?: (index: string) => void,
-    tabPosition?: flexDirection,
+    size?: size,
+    tabPosition?: position,
+    onChange?: (before: string, after: string) => void,
 }
 
 export const Tabs: React.FC<TabsProps> = (props) => {
     const {
+        defaultActiveIndex = '1',
+        size = 'default',
         type = 'line',
         tabPosition = "top",
-        defaultActiveKey,
         onChange,
     } = props
 
-    // useEffect只在更新时执行操作
-    // https://stackoverflow.com/questions/55075604/react-hooks-useeffect-only-on-update
-    const isInitialMount = useRef(true)
-
-    const [activeKey, setActiveKey] = useState(defaultActiveKey || '1')
+    const [activeIndex, setActiveIndex] = useState(defaultActiveIndex)
     useEffect(() => {
-        setContentLeft(activeKey === '1' ? '0' : `-${parseInt(activeKey) - 1}00%`)
-        if (isInitialMount.current) {
-            isInitialMount.current = false
-        } else {
-            if (onChange) onChange(activeKey)
-        }
-    }, [activeKey])
+        setContentLeft(activeIndex === '1' ? '0' : `-${parseInt(activeIndex) - 1}00%`)
+    }, [activeIndex])
     function handleToggle(index: string) {
-        setActiveKey(index)
+        setActiveIndex(index)
+        if (onChange) onChange(activeIndex, index)
     }
     
     const [contentLeft, setContentLeft] = useState('0')
 
-    function calcTabDirection(tabPosition: flexDirection) {
-        switch(tabPosition) {
-            case "top":
-                return "column"
-            case "bottom":
-                return "column-reverse"
-            case "left":
-                return "row"
-            case "right":
-                return "row-reverse"
-        }
-    }
 
-    function calcLabelDirection(tabPosition: flexDirection) {
-        if (tabPosition === 'top' || tabPosition === 'bottom') return 'row'
-        return 'column' 
+    const posToDirec = {
+        'top': 'column',
+        'bottom': 'column-reverse',
+        'left': 'row',
+        'right': 'row-reverse',
+    }
+    
+    function calcTabDirection(tabPosition: position = 'top'): string {
+        return posToDirec[tabPosition]
     }
 
     function labelContainerClass() {
@@ -69,45 +58,52 @@ export const Tabs: React.FC<TabsProps> = (props) => {
             'tab-label-container-card': type === 'card',
         })
     }
-
-    function contentClass(key: string) {
+    
+    function contentContainerClass(index: string) {
         return classNames({
             'tab-content': true,
-            'tab-content-active': activeKey === key
+            'tab-content-active': activeIndex === index
         })
     }
 
+    
+
+    const direction = posToDirec[tabPosition]
     return (
-        <div className="tabs" style={{ flexDirection: calcTabDirection(tabPosition)}}>
+        <div className="tabs" style={{flexDirection: calcTabDirection(tabPosition)}}>
             <div className={labelContainerClass()}>
                 {
                     React.Children.map(props.children as React.ReactNode, function (child: any, i) {
                         const {
                             label,
-                            index,
                             children,
+                            ...rest
                         } = child.props
 
-                        return <Label index={index} activeIndex={activeKey} onClick={handleToggle}>{label}</Label>
+                        return <Label onClick={handleToggle} size={size} activeIndex={activeIndex} {...rest}>{label}</Label>
                     })
+                }
+                {
+                    type === 'card' && 
+                    <div className="tab-label-plus">
+                        <div className="tab-label-icon"><svg t="1590675331645" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1131" width="16" height="16"><path d="M185.89696 532.2752l286.72 0 0 286.72c0 11.32544 9.17504 20.48 20.48 20.48s20.48-9.15456 20.48-20.48l0-286.72 286.72 0c11.30496 0 20.48-9.15456 20.48-20.48s-9.17504-20.48-20.48-20.48l-286.72 0 0-286.72c0-11.32544-9.17504-20.48-20.48-20.48s-20.48 9.15456-20.48 20.48l0 286.72-286.72 0c-11.30496 0-20.48 9.15456-20.48 20.48S174.592 532.2752 185.89696 532.2752z" p-id="1132"></path></svg></div>
+                    </div>
                 }
             </div>
             <div className="tab-content-container" style={{ marginLeft: contentLeft}}>
                 {
                     React.Children.map(props.children as React.ReactNode, function (child: any, i) {
                         const {
-                            label,
                             index,
                             children,
                         } = child.props
-                        return <div className={contentClass(index)}>{children}</div>
+                        return <div className={contentContainerClass(index)}>{children}</div>
                     })
                 }
             </div>
         </div>
     )
 }
-
 
 
 
@@ -128,32 +124,48 @@ export const TabPane: React.FC<TabPaneProps> = (props) => {
 
 
 
+
+
 // 内部组件
 interface LabelProps {
     index: string,
     activeIndex: string,
     onClick: (index: string) => void,
+    disabled?: boolean,
+    size?: size,
 }
-const Label: React.FC<LabelProps> = ({
-    index,
-    activeIndex,
-    onClick,
-    children,
-}) => {
+const Label: React.FC<LabelProps> = (props) => {
+    const {
+        size,
+        index,
+        activeIndex,
+        onClick,
+        disabled,
+        children,
+    } = props
+    console.log(activeIndex);
+    
+
     const [active, setActive] = useState(false)
 
     useEffect(() => {
         setActive(index === activeIndex)
     }, [activeIndex])
-    
-    
 
-    const elClass = classNames({
+    const innerClass = classNames({
         'tab-label': true,
-        'tab-label-active': active
+        'tab-label-active': active,
+        'tab-label-disabled': disabled
     })
 
+    const className = `${innerClass} tab-label-${size}`
+
+
+    function handleClick() {
+        if (!disabled) onClick(index)
+    }
+
     return (
-        <div className={elClass} onClick={() => onClick(index)}>{children}</div>
+        <div className={className} onClick={handleClick}>{children}</div>
     )
 }
